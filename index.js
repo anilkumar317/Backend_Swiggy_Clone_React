dbaae25e1a61b170c4cd7248af165231d810003f
ANIL KUMAR <nangianilkumar216@gmail.com>
2025-03-25 10:03:15 +0530

    to fix Cross Origin related issue in index.js file

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 
 const PORT = process.env.PORT || 4000;
 
